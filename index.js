55579c1a3d56310f33655826e60864e5509bad8c
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2020-10-27 14:27:48 -0400

    termometro y acelerometro

--- index.js
+++ index.js
@@ -1,65 +1,57 @@
-'use strict';
+"use strict";
 
-const {
-  EtherPortClient
-} = require('etherport-client');
-
-//"reset":"firmata-party uno --debug",
-const { Accelerometer, Board,Thermometer } = require("johnny-five");
+const { EtherPortClient } = require("etherport-client");
+const { Accelerometer, Board, Thermometer } = require("johnny-five");
 const board = new Board({
   port: new EtherPortClient({
-    host: '192.168.0.11',
-    port: 3030
+    host: "192.168.0.11",
+    port: 3030,
+    baudrate: 9600,
+    buffersize: 1,
   }),
-  repl: false
+  repl: true,
 });
 
 board.on("ready", () => {
-
   const thermometer = new Thermometer({
     controller: "LM35",
-    pin:"A0"
+    pin: "A0",
   });
-  thermometer.on("change", () => {
-    const {celsius, fahrenheit, kelvin} = thermometer;
+  thermometer.on("data", () => {
+    const { celsius, fahrenheit, kelvin } = thermometer;
     console.log("Thermometer");
-    console.log("  celsius      : ", celsius/2);
-    console.log("  fahrenheit   : ", fahrenheit);
-    console.log("  kelvin       : ", kelvin);
+    console.log("  celsius      : ", celsius / 2);
+    // console.log("  fahrenheit   : ", fahrenheit);
+    // console.log("  kelvin       : ", kelvin);
     console.log("--------------------------------------");
   });
 
-  // const accelerometer = new Accelerometer({
-  //   controller: "MPU6050",
-  // });
+  const accelerometer = new Accelerometer({
+    controller: "MPU6050",
+  });
 
-  // accelerometer.on("change", () => {
-  //   const {
-  //     acceleration,
-  //     inclination,
-  //     orientation,
-  //     pitch,
-  //     roll,
-  //     x,
-  //     y,
-  //     z,
-  //   } = accelerometer;
+  accelerometer.on("data", () => {
+    const {
+      acceleration,
+      inclination,
+      orientation,
+      pitch,
+      roll,
+      x,
+      y,
+      z,
+    } = accelerometer;
 
-  //       console.log("Accelerometer:");
-  //       console.log("  x            : ", x);
-  //       console.log("  y            : ", y);
-  //       console.log("  z            : ", z);
-  //       console.log("  pitch        : ", pitch);
-  //       console.log("  roll         : ", roll);
-  //       console.log("  acceleration : ", acceleration);
-  //       console.log("  inclination  : ", inclination);
-  //       console.log("  orientation  : ", orientation);
-  //       console.log(new Date());
-  //       console.log("--------------------------------------");
-    
-
-
-  // });
-
-
+    console.log("Accelerometer:");
+    console.log("  x            : ", x);
+    console.log("  y            : ", y);
+    console.log("  z            : ", z);
+    console.log("  pitch        : ", pitch);
+    console.log("  roll         : ", roll);
+    console.log("  acceleration : ", acceleration);
+    console.log("  inclination  : ", inclination);
+    console.log("  orientation  : ", orientation);
+    console.log(new Date());
+    console.log("--------------------------------------");
+  });
 });
