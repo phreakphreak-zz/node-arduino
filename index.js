598ec1178454d3c21b569a6cfcec404ce58475ea
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2020-10-27 20:28:16 -0400

    config axios

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 "use strict";
 const http = require("http");
+const axios = require("axios").default;
 const express = require("express");
 const socketIO = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO.listen(server);
-server.listen(4000,()=>{
+server.listen(4000, () => {
   console.log("server on port 4000");
-  
-})
+});
 
 const { EtherPortClient } = require("etherport-client");
 const { Accelerometer, Board, Thermometer } = require("johnny-five");
@@ -23,29 +23,37 @@ const board = new Board({
   repl: true,
 });
 
+
+const dataThermometer = {};
+const dataAccelerometer = {};
+
+
 board.on("ready", () => {
   board.samplingInterval(1000);
+
   const thermometer = new Thermometer({
     controller: "LM35",
     pin: "A0",
   });
 
-
   const accelerometer = new Accelerometer({
     controller: "MPU6050",
   });
 
-
-  const data = {};
-
-    thermometer.on("change", async () => {
+  thermometer.on("change", async () => {
     const { celsius, fahrenheit, kelvin } = await thermometer;
-      data.thermometer ={
-        celsius:celsius,
-        fahrenheit:fahrenheit,
-        kelvin:kelvin,
-      }
-    console.log(`${celsius/2} C - ${fahrenheit} F - ${kelvin} K`);
+    dataThermometer.thermometer = {
+      celsius: celsius,
+      fahrenheit: fahrenheit,
+      kelvin: kelvin,
+    };
+    axios.post('http://192.168.0.12/api/devices/thermometer',dataThermometer)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   });
 
   accelerometer.on("change", async () => {
@@ -60,29 +68,27 @@ board.on("ready", () => {
       z,
     } = await accelerometer;
 
-    data.accelerometer = {
-      x:x,
-      y:y,
-      z:z,
-      roll:roll,
-      pitch:pitch,
-      orientation:orientation,
-      inclination:inclination,
-      acceleration:acceleration,
-    }
-    console.log("Accelerometer:");
-    console.log("  x            : ", x);
-    console.log("  y            : ", y);
-    console.log("  z            : ", z);
-    console.log("  pitch        : ", pitch);
-    console.log("  roll         : ", roll);
-    console.log("  acceleration : ", acceleration);
-    console.log("  inclination  : ", inclination);
-    console.log("  orientation  : ", orientation);
-    console.log(new Date());
-    console.log("--------------------------------------");
+    dataAccelerometer.accelerometer = {
+      x: x,
+      y: y,
+      z: z,
+      roll: roll,
+      pitch: pitch,
+      orientation: orientation,
+      inclination: inclination,
+      acceleration: acceleration,
+    };
+    axios.post('http://192.168.0.12/api/devices/accelerometer',dataAccelerometer)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   });
-
-
-//   io.emit("data",data);
 });
+
+
+
+
+
