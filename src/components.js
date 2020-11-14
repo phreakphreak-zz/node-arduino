d4cd41538359b181a193288d9e649179b0702218
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2020-11-14 00:36:40 -0400

    nada

--- src/components.js
+++ src/components.js
@@ -19,22 +19,24 @@ const gyro = new Gyro({
 
 const accelerometer = new Accelerometer({
   controller: "MPU6050",
-  sensitivity:4096
+  sensitivity: 4096,
 });
 const thermometer = new Thermometer({
   controller: "LM35",
   pin: "A0",
 });
 const device = {
-    board: "Wemos D1",
-    moduleWifi: "ESP8266",
-    macAddress: "84:0D:8E:A3:B1:EA",
-    thermometer: "LM35",
-    accelerometer: "MPU6050",
-  };
+  board: "Wemos D1",
+  moduleWifi: "ESP8266",
+  macAddress: "84:0D:8E:A3:B1:EA",
+  thermometer: "LM35",
+  accelerometer: "MPU6050",
+};
 
-module.exports.device = device;
-module.exports.board = board;
-module.exports.accelerometer = accelerometer;
-module.exports.thermometer = thermometer;
-module.exports.gyro = gyro;
+module.exports = {
+  device,
+  board,
+  accelerometer,
+  thermometer,
+  gyro,
+};
