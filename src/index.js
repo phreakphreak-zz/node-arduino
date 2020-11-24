f24e89d12fed8f5401522527d8dd63ae116dc9a1
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2020-11-23 20:11:47 -0400

    config axios

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@
 const { EtherPortClient } = require("etherport-client");
 const { Board, Accelerometer, Thermometer, Gyro } = require("johnny-five");
 const axios = require("axios").default;
-
+const device = {
+  board: "Wemos D1",
+  moduleWifi: "ESP8266",
+  macAddress: "84:0D:8E:A3:B1:EA",
+  thermometer: "LM35",
+  accelerometer: "MPU6050",
+};
 const board = new Board({
   port: new EtherPortClient({
     host: "192.168.0.8",
@@ -13,7 +19,11 @@ const board = new Board({
   repl: false,
 });
 board.samplingInterval(1000);
-board.on("ready", () => {
+board.on("ready", async () => {
+  //const { data } = await axios.post(URI1, device);
+
+  //device.deviceId = data.deviceId;
+  //board.id = data.deviceId;
   const thermometer = new Thermometer({
     controller: "LM35",
     pin: "A0",
