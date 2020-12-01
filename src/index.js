7287f50f86fba7db15f0c756d1f2af98905c4b00
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2020-12-01 17:08:22 -0400

    send to api

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const { EtherPortClient } = require("etherport-client");
 const { Board, Accelerometer, Thermometer, Gyro } = require("johnny-five");
 const axios = require("axios").default;
 const BASE = "http://localhost:3000/api";
+//const BASE = "https://cow-api-services.herokuapp.com/api";
 const URI_DEV = `${BASE}/device`;
 const URI_ACCE = `${BASE}/accelerometer`;
 const URI_GYRO = `${BASE}/gyro`;
 const URI_THER = `${BASE}/thermometer`;
 const device = {
+  number: 1,
   board: "Wemos D1",
   moduleWifi: "ESP8266",
   macAddress: "84:0D:8E:A3:B1:EA",
@@ -25,6 +27,15 @@ const board = new Board({
 });
 board.samplingInterval(1000);
 board.on("ready", async () => {
+  const { _id } = await axios.get(URI_DEV, {
+    params: {
+      id: board.id,
+    },
+  });
+  if (!_id) {
+    const { deviceId } = await axios.post(URI_DEV, device);
+    board.id = deviceId;
+  }
 
   const thermometer = new Thermometer({
     controller: "LM35",
@@ -42,11 +53,18 @@ board.on("ready", async () => {
 
   thermometer.on("change", async () => {
     const { C, F, K } = await thermometer;
-    console.log("thermometer");
-    console.log(`Celsius: ${C}`);
-    console.log(`Fahrenheit: ${F}`);
-    console.log(`Kelvin: ${K}`);
+    // console.log("thermometer");
+    // console.log(`Celsius: ${C}`);
+    // console.log(`Fahrenheit: ${F}`);
+    // console.log(`Kelvin: ${K}`);
+    const { message } = await axios.post(URI_THER, {
+      C,
+      K,
+      F,
+      deviceId: board.id,
+    });
 
+    console.log("thermometer:", message);
   });
 
   accelerometer.on("change", async () => {
@@ -60,29 +78,52 @@ board.on("ready", async () => {
       orientation,
       acceleration,
     } = await accelerometer;
-    console.log("Acclerometer");
-    console.log(`x: ${x}`);
-    console.log(`y: ${y}`);
-    console.log(`z: ${z}`);
-    console.log(`pitch: ${pitch}`);
-    console.log(`roll: ${roll}`);
-    console.log(`inclination: ${inclination}`);
-    console.log(`orientation: ${orientation}`);
-    console.log(`acceleration: ${acceleration}`);
+    // console.log("Acclerometer");
+    // console.log(`x: ${x}`);
+    // console.log(`y: ${y}`);
+    // console.log(`z: ${z}`);
+    // console.log(`pitch: ${pitch}`);
+    // console.log(`roll: ${roll}`);
+    // console.log(`inclination: ${inclination}`);
+    // console.log(`orientation: ${orientation}`);
+    // console.log(`acceleration: ${acceleration}`);
+    const {message} = await axios.post(URI_ACCE, {
+      x,
+      y,
+      z,
+      pitch,
+      roll,
+      inclination,
+      orientation,
+      acceleration,
+      deviceId: board.id,
+    });
 
-
+    console.log("accelerometer:", message);
   });
 
   gyro.on("change", async () => {
     const { x, y, z, pitch, roll, yaw, isCalibrated } = await gyro;
-    console.log("Gyro")
-    console.log(`x: ${x}`);
-    console.log(`y: ${y}`);
-    console.log(`z: ${z}`);
-    console.log(`pitch: ${pitch}`);
-    console.log(`roll: ${roll}`);
-    console.log(`yaw: ${yaw}`);
-    console.log(`isCalibrated: ${isCalibrated}`);
+    // console.log("Gyro");
+    // console.log(`x: ${x}`);
+    // console.log(`y: ${y}`);
+    // console.log(`z: ${z}`);
+    // console.log(`pitch: ${pitch}`);
+    // console.log(`roll: ${roll}`);
+    // console.log(`yaw: ${yaw}`);
+    // console.log(`isCalibrated: ${isCalibrated}`);
 
+    const { message } = await axios.post(URI_ACCE, {
+      x,
+      y,
+      z,
+      pitch,
+      roll,
+      yaw,
+      isCalibrated,
+      deviceId: board.id,
+    });
+
+    console.log("gyro:", message);
   });
 });
