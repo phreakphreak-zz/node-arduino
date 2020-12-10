2a6c1f4a551a31c465367f89f35d2c617e85b081
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2020-12-10 11:49:19 -0400

    Last details validation data array

--- src/index.js
+++ src/index.js
@@ -2,12 +2,33 @@
 const { EtherPortClient } = require("etherport-client");
 const { Board, Accelerometer, Thermometer, Gyro } = require("johnny-five");
 const axios = require("axios").default;
-const BASE = "http://localhost:3000/api";
-//const BASE = "https://cow-api-services.herokuapp.com/api";
-const URI_DEV = `${BASE}/device`;
+//const BASE = "http://192.168.0.44:3000";
+const BASE = "https://cow-services.herokuapp.com";
+const URI_DEV = `${BASE}/devices`;
 const URI_ACCE = `${BASE}/accelerometer`;
 const URI_GYRO = `${BASE}/gyro`;
 const URI_THER = `${BASE}/thermometer`;
+let oldAccelerometer;
+let oldGyro;
+let oldThermometer;
+
+async function sendData(oldValues, newValues, uri, body, component) {
+  if (!oldValues) {
+    const { data } = await axios.post(uri, body);
+    oldValues = newValues;
+    console.log(`${component}: ${data._id}`);
+  } else {
+    const res = newValues.every(function (v, i) {
+      return v === oldValues[i];
+    });
+    if (!res) {
+      const { data } = await axios.post(uri, body);
+      oldValues = newValues;
+      console.log(`${component}: ${data._id}`);
+    }
+  }
+}
+
 const device = {
   number: 1,
   board: "Wemos D1",
@@ -15,6 +36,7 @@ const device = {
   macAddress: "84:0D:8E:A3:B1:EA",
   thermometer: "LM35",
   accelerometer: "MPU6050",
+  gyro: "MPU6050",
 };
 const board = new Board({
   port: new EtherPortClient({
@@ -25,18 +47,12 @@ const board = new Board({
   }),
   repl: false,
 });
+board.id = "5fd147e062a6f65fa202b68c";
+
 board.samplingInterval(1000);
 board.on("ready", async () => {
-  const { _id } = await axios.get(URI_DEV, {
-    params: {
-      id: board.id,
-    },
-  });
-  if (!_id) {
-    const { deviceId } = await axios.post(URI_DEV, device);
-    board.id = deviceId;
-  }
-
+  console.log(board.id);
+  
   const thermometer = new Thermometer({
     controller: "LM35",
     pin: "A0",
@@ -53,18 +69,20 @@ board.on("ready", async () => {
 
   thermometer.on("change", async () => {
     const { C, F, K } = await thermometer;
-    // console.log("thermometer");
-    // console.log(`Celsius: ${C}`);
-    // console.log(`Fahrenheit: ${F}`);
-    // console.log(`Kelvin: ${K}`);
-    const { message } = await axios.post(URI_THER, {
+    let newThermometer = [C, F, K];
+    let body = {
       C,
-      K,
       F,
+      K,
       deviceId: board.id,
-    });
-
-    console.log("thermometer:", message);
+    };
+    await sendData(
+      oldThermometer,
+      newThermometer,
+      URI_THER,
+      body,
+      "thermometer"
+    );
   });
 
   accelerometer.on("change", async () => {
@@ -78,16 +96,7 @@ board.on("ready", async () => {
       orientation,
       acceleration,
     } = await accelerometer;
-    // console.log("Acclerometer");
-    // console.log(`x: ${x}`);
-    // console.log(`y: ${y}`);
-    // console.log(`z: ${z}`);
-    // console.log(`pitch: ${pitch}`);
-    // console.log(`roll: ${roll}`);
-    // console.log(`inclination: ${inclination}`);
-    // console.log(`orientation: ${orientation}`);
-    // console.log(`acceleration: ${acceleration}`);
-    const {message} = await axios.post(URI_ACCE, {
+    let body = {
       x,
       y,
       z,
@@ -97,23 +106,78 @@ board.on("ready", async () => {
       orientation,
       acceleration,
       deviceId: board.id,
-    });
+    };
+    let newAccelerometer = [
+      x,
+      y,
+      z,
+      pitch,
+      roll,
+      inclination,
+      orientation,
+      acceleration,
+    ];
 
-    console.log("accelerometer:", message);
+    if (!oldAccelerometer) {
+      const { data } = await axios.post(URI_ACCE, {
+        x,
+        y,
+        z,
+        pitch,
+        roll,
+        inclination,
+        orientation,
+        acceleration,
+        deviceId: board.id,
+      });
+      oldAccelerometer = [
+        x,
+        y,
+        z,
+        pitch,
+        roll,
+        inclination,
+        orientation,
+        acceleration,
+      ];
+
+      console.log("accelerometer:", data._id);
+    } else {
+      const res = newAccelerometer.every(function (v, i) {
+        return v === oldAccelerometer[i];
+      });
+
+      if (!res) {
+        const { data } = await axios.post(URI_ACCE, {
+          x,
+          y,
+          z,
+          pitch,
+          roll,
+          inclination,
+          orientation,
+          acceleration,
+          deviceId: board.id,
+        });
+        oldAccelerometer = [
+          x,
+          y,
+          z,
+          pitch,
+          roll,
+          inclination,
+          orientation,
+          acceleration,
+        ];
+
+        console.log("accelerometer:", data._id);
+      }
+    }
   });
 
   gyro.on("change", async () => {
     const { x, y, z, pitch, roll, yaw, isCalibrated } = await gyro;
-    // console.log("Gyro");
-    // console.log(`x: ${x}`);
-    // console.log(`y: ${y}`);
-    // console.log(`z: ${z}`);
-    // console.log(`pitch: ${pitch}`);
-    // console.log(`roll: ${roll}`);
-    // console.log(`yaw: ${yaw}`);
-    // console.log(`isCalibrated: ${isCalibrated}`);
-
-    const { message } = await axios.post(URI_GYRO, {
+    let body = {
       x,
       y,
       z,
@@ -122,8 +186,8 @@ board.on("ready", async () => {
       yaw,
       isCalibrated,
       deviceId: board.id,
-    });
-
-    console.log("gyro:", message);
+    };
+    let newGyro = [x, y, z, pitch, roll, yaw, isCalibrated];
+    await sendData(oldGyro, newGyro, URI_GYRO, body, "gyro");
   });
 });
